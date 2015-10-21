a7ac10369c2e14f9d4541579614012f8caf9bad4
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-10-21 23:21:42 +0200

    basic layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
+import Header from './Header/Header';
+import Maps from './Maps/Maps';
+import SideBar from './SideBar/SideBar';
 import * as actionCreators from './../../action-creators';
 
 export const App = React.createClass({
   mixins: [PureRenderMixin],
   render: () => {
-    return (<div>hi
+    return (<div>
+      <Header />
+      <div className="content">
+        <div className="left">
+          <Maps />
+        </div>
+        <div className="right">
+          <SideBar />
+        </div>
+      </div>
     </div>);
   },
 });
